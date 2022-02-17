ca90bb3e25bdbc710e99d11492b8a1a48cf88c24
Ahsan Ali <ahsanbutt515@icloud.com>
2022-02-17 13:53:51 +0500

    i added bootstrap and link it with project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       Hellow world...Is it working /.? Commiting git.
+      <button className="btn btn-danger">
+Connecting react
+      </button>
     </div>
   );
 }
