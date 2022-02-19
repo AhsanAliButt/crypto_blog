58db5f2a78c8cf890217f4a9880edc6f5f991cc0
Ahsan Ali <ahsanbutt515@icloud.com>
2022-02-19 17:21:21 +0500

    Update some details almost complete modify leter

--- src/Assignment1.js
+++ src/Assignment1.js
@@ -7,10 +7,14 @@ const Assignment1 = (props) => {
       <div className="container">
         <div className="row">
           <div className="col col1 text-start">
-            <h1>Welcome to<br></br> {props.blogname}</h1>
-            <br></br>
-            <p className='g1'>Cryptocurrency is stored in digital wallets. Cryptocurrency received its name because it uses encryption to verify transactions.Cryptocurrencies such as bitcoin and Ethereum were designed as a way to make payments without relying on traditional modes such as currency notes, debit cards, credit cards or checks. The bitcoin white paper, which set off the cryptocurrency revolution, envisions an electronic payment system that allows “any two willing parties to transact directly with each other without the need for a trusted third party,” cutting governments and banks out of the financial loop. The website Pymnts claims, “Blockchain IS the future of the payments industry,” a reference to the computational technology that undergirds cryptocurrencies.
-</p>
+            <div className="card bg-dark text-white">
+  <img src="https://phantom-marca.unidadeditorial.es/7c4ccd41cb946352fe6e15a6c32773a1/crop/0x0/2041x1150/resize/1320/f/jpg/assets/multimedia/imagenes/2022/01/07/16415655339687.jpg" className="card-img image1" alt="..."/>
+  <div className="card-img-overlay">
+    <h1 className="card-title">Welcome to<br></br> {props.blogname}</h1><br></br>
+    <p className="card-text g1">Cryptocurrency is stored in digital wallets. Cryptocurrency received its name because it uses encryption to verify transactions.Cryptocurrencies such as bitcoin and Ethereum were designed as a way to make payments without relying on traditional modes such as currency notes, debit cards, credit cards or checks. The bitcoin white paper, which set off the cryptocurrency revolution, envisions an electronic payment system that allows “any two willing parties to transact directly with each other without the need for a trusted third party,” cutting governments and banks out of the financial loop. The website Pymnts claims, “Blockchain IS the future of the payments industry,” a reference to the computational technology that undergirds cryptocurrencies.
+            </p>
+  </div>
+</div>
           </div>
         </div>
         {/* Navigation section ends */}
@@ -23,7 +27,8 @@ const Assignment1 = (props) => {
                 <img src="https://www.notebookcheck.net/fileadmin/Notebooks/News/_nc3/Crypto_News_Shiba_Inu_Coin_Ethereum_Hack.png" className="i1" alt="..." />
 
                 <h5 className="card-title m-2 p-2">Shiba inu</h5>
-                <p className="card-text m-2 p-2">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <p className="card-text m-2 p-2">.Shiba Inu is an Ethereum-based altcoin that features the Shiba Inu hunting dog as its mascot. ... Shiba Inu was created in August 2020 by an individual or group called Ryoshi. Shiba Inu's price peaked more than tenfold on Oct. 29, 2021, giving it a market capitalization of $41 billion.</p>
+                <p class="card-text m-2 p-2"><small class="text-muted">Last updated 3 mins ago</small></p>
               </div>
             </div>
 
@@ -34,7 +39,8 @@ const Assignment1 = (props) => {
                 <img src="http://coinlive.me/wp-content/uploads/2021/09/Evaluation-of-the-Gala-Games-platform-GALA-coin-Latest-information.jpg" className="i1" alt="..." />
 
                 <h5 className="card-title m-2 p-2">Gala Metaverse</h5>
-                <p className="card-text m-2 p-2">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <p className="card-text m-2 p-2">The GALA tokens are the digital utility tokens of the Gala Games Ecosystem that operates on the Ethereum blockchain and conforms to the ERC-20 standard. More specifically, the non-refundable utility token is cryptographically secured and used as a medium of exchange between participants in the ecosystem.</p>
+                <p class="card-text m-2 p-2"><small class="text-muted">Last updated 3 mins ago</small></p>
               </div>
             </div>
 
@@ -51,11 +57,36 @@ const Assignment1 = (props) => {
           </div>
           <div className="row mt-3 side1 bg-light">
             <div className="col text-start bg1">
-              <h5>About</h5>
+              <h5 className="fw-bold fst-italic mt-2">About</h5><br></br>
+              <p className='fw-bold'> In 2009, the first decentralized cryptocurrency, bitcoin, was created by presumably pseudonymous developer Satoshi Nakamoto. It used SHA-256, a cryptographic hash function, in its proof-of-work scheme.</p>
             </div>
+            <div className="row side1 mt-5 p-0">
+              <div className="col text-start bg1">
+                <h5 className="fw-bold fst-italic mt-2">Archives</h5><br></br>
+                <br></br>
+                <ul class="list-unstyled ms-3">
+                  <li><a href="#" class="link-primary">January 2010</a></li>
+                  <li><a href="#" class="link-primary">January 2011</a></li>
 
+                    <li><a href="#" class="link-primary">January 2012</a></li>
+                    <li><a href="#" class="link-primary">January 2013</a></li>
+
+                  <li><a href="#" class="link-primary">January 2014</a></li>
+                  <li><a href="#" class="link-primary">January 2015</a></li>
+                </ul>
+              </div>
+            </div>
           </div>
         </div>
+        {/* Footer Starts */}
+        <footer>
+          <div className="row p-0 border border-dark border-1 mt-2">
+            <div className="col p-0">
+              its footer
+            </div>
+          
+          </div>
+        </footer>
       </div>
     </>
 
