bebeb7370866510d8d45955f4e2e9090586bf1b2
Ahsan Ali <ahsanbutt515@icloud.com>
2022-02-23 03:20:42 +0500

    update some frontEnd

--- src/components/Links/Links.js
+++ src/components/Links/Links.js
@@ -1,7 +1,7 @@
 export const Link = (props) => {
     return (
-        <li className="nav-item m-2">
-            <a className="nav-link active fw-bold" aria-current="page" href="/">{props.linkname}</a>
+        <li className="nav-item m-0 p-0 ms-3 mt-3">
+            <a className="nav-link active fw-bold fs-4" aria-current="page" href="/">{props.linkname}</a>
 
         </li>
 
--- src/components/navbar/navBar.js
+++ src/components/navbar/navBar.js
@@ -6,15 +6,16 @@ export const NavBar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bn sticky-top border border-primary" style={{borderRadius:'5px'}}>
             <div className="container-fluid">
-                <a classNameName="navbar-brand" href="/">
+                <a className="navbar-brand" href="/">
 
-                    <img src={logo} alt="" width="60" height="60" classNameName="d-inline-block fw-bold text-white" /> KryptoKnight
+                    <img src={logo} alt="" width="70" height="70" className="rounded-circle d-inline-block fw-bold text-white" />
                 </a>
+                <p className="l1 fw-bold fs-2 m-0 p-0">KryptoKnight</p>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <div className="collapse navbar-collapse navMenu" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0 navMenu">
                         <Link linkname="Home" />
                         <Link linkname="Link" />
                         <li className="nav-item dropdown m-2">
@@ -31,7 +32,7 @@ export const NavBar = () => {
                         <Link linkname="Contact Us" />
                     </ul>
                     <form className="d-flex">
-                        <input className="form-control m-2 " type="search" placeholder="Search" aria-label="Search" />
+                        <input className="form-control m-1 " type="search" placeholder="Search" aria-label="Search" />
                         <Button buttonname="Search" color="gray" textColor="white" />
                     </form>
                 </div>
