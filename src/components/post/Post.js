2b1c748e8ba7a28b0caf799d6f4dbd13f49197ac
Ahsan Ali <ahsanbutt515@icloud.com>
2022-02-27 18:39:27 +0500

    Just made some trash code clear

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -1,24 +1,8 @@
 import ReactReadMoreReadLess from 'react-read-more-read-less';
 // import cardData from "../variables/variables.js"
 const Post = (props) => {
-    // {cardData.map((item,index)=>{
-    //     return(
-    //         <Post data={data} />
-    //     )
-    // })}    
-    return (
         
-
-        // <div className="col-sm-12 col-md-6 col-lg-3 text-start col2 p-0 m-0 p-4 mt-4">
-        //     <div className="card">
-        //         <img src={props.coinimg} className="card-img-top i1" alt="..." />
-        //         <div className="card-body bg-light">
-        //             <h5 className="card-title">{props.coinname}</h5>
-        //             <h5 className="card-title">{"current-price"} <button className="btn btn-danger ms-2"> Buy Now </button></h5>
-        //             <p className="card-text fw-bold"><ReactReadMoreReadLess ReadMoreclassName="readMoreclassName" charLimit={100} readMoreText='Read More' readLessText="ReadLess" readMoreStyle={{ color: "blue", cursor: "pointer" }} readLessStyle={{ color: "red", curson: 'pointer' }}>{props.coinintro}</ReactReadMoreReadLess></p>
-        //         </div>
-        //     </div>
-        // </div>
+    return (
         <div className="col-sm-12 col-md-6 col-lg-3 text-start col2 p-0 m-0 p-4 mt-4">
             <div className="card">
                 <img src={props.data.image} className="card-img-top i1" alt="..." />
@@ -29,16 +13,7 @@ const Post = (props) => {
                 </div>
             </div>
         </div>
-        // <div className="col-sm-12 col-md-6 col-lg-3 text-start col2 p-0 m-0 p-4 mt-4">
-        //     <div className="card">
-        //         <img src={item.image} className="card-img-top i1" alt="..." />
-        //         <div className="card-body bg-light">
-        //             <h5 className="card-title">{item.name}</h5>
-        //             <h5 className="card-title">{item.price} <button className="btn btn-danger ms-2"> Buy Now </button></h5>
-        //             <p className="card-text fw-bold"><ReactReadMoreReadLess ReadMoreclassName="readMoreclassName" charLimit={100} readMoreText='Read More' readLessText="ReadLess" readMoreStyle={{ color: "blue", cursor: "pointer" }} readLessStyle={{ color: "red", curson: 'pointer' }}>{item.disc}</ReactReadMoreReadLess></p>
-        //         </div>
-        //     </div>
-        // </div>
+    
     )
 }
 
--- src/components/post/Posts.js
+++ src/components/post/Posts.js
@@ -1,26 +1,14 @@
 import './postStyle.js';
 import Post from './Post.js';
 import cardData from "../variables/variables.js"
-// import { img, names, intro } from "../variables/variables.js";
+
 export function Posts() {
-  //   {cardData.map((item,index)=>{
-  //     return(
-  //         <Post data={item} />
-  //     )
-  // })}
+  
   return (
 
-    //  <div className="row mt-5">
+  
     <div class="row row-cols-1 row-cols-md-3 g-4 my-5">
-      {/* U can post By following Method */}
-      {/* <Posts coinname={"Name"} coinintro={intro.name} img = {img.name}/> */}
-      {/* You can find all Coin names Names intros and imgs in variable.js file */}
-
-      {/* <Post coinname={names.gala} coinintro={intro.gala} coinimg={img.gala}/>
-          <Post coinname={names.sand} coinintro={intro.sand} coinimg={img.sand}/>
-          <Post coinname={names.sys} coinintro={intro.sys} coinimg={img.sys}/>
-          <Post coinname={names.ethereum} coinintro={intro.ethereum} coinimg={img.ethereum}/>
-          <Post coinname={names.sand} coinintro={intro.sand} coinimg={img.sand}/> */}
+      
       {cardData.map((item, index) => {
         return (
           <Post data={item} />
